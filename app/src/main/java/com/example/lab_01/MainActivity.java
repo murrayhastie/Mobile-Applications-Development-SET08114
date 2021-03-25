2202c1765570ef11f979fa44f480b9983a55be7c
murrayhastie <murrayhastie@hotmail.com>
2021-03-25 11:10:54 +0000

    Lab_01 Complete

--- app/src/main/java/com/example/lab_01/MainActivity.java
+++ app/src/main/java/com/example/lab_01/MainActivity.java
@@ -2,13 +2,10 @@ package com.example.lab_01;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
-import android.widget.Button;
-import android.widget.CheckBox;
+import android.widget.ImageView;
+import android.widget.RadioButton;
 import android.widget.TextView;
-import android.widget.Toast;
-import java.util.Date;
 
 public class MainActivity extends AppCompatActivity
 {
@@ -18,24 +15,36 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        CheckBox cbox1 = (CheckBox) findViewById(R.id.check_option_1);
-        cbox1.setOnClickListener(startListener);
+        RadioButton rb0 = (RadioButton) findViewById(R.id.radioButton0);
+        rb0.setOnClickListener(radioGroupClick);
 
-        CheckBox cbox2 = (CheckBox) findViewById(R.id.check_option_2);
-        cbox2.setOnClickListener(startListener);
+        RadioButton rb1 = (RadioButton) findViewById(R.id.radioButton1);
+        rb1.setOnClickListener(radioGroupClick);
 
-        CheckBox cbox3 = (CheckBox) findViewById(R.id.check_option_3);
-        cbox3.setOnClickListener(startListener);
+        RadioButton rb2 = (RadioButton) findViewById(R.id.radioButton2);
+        rb2.setOnClickListener(radioGroupClick);
+
+
     }
 
-    private View.OnClickListener startListener = new View.OnClickListener()
+    private View.OnClickListener radioGroupClick = new View.OnClickListener()
     {
         public void onClick(View v)
         {
-            CheckBox cbox1 = (CheckBox) findViewById(R.id.check_option_1);
-            if (cbox1.isChecked())
+            RadioButton rb = (RadioButton) v;
+            ImageView iv = (ImageView) findViewById(R.id.imageView);
+
+            if (rb.getText().equals("Option 1"))
             {
-                Toast.makeText(MainActivity.this, "Option 1", Toast.LENGTH_LONG).show();
+                iv.setImageResource(R.mipmap.hopewell_rocks);
+            }
+            else if (rb.getText().equals("Option 2"))
+            {
+                iv.setImageResource(R.mipmap.niagara_falls);
+            }
+            else if (rb.getText().equals("Option 3"))
+            {
+                iv.setImageResource(R.mipmap.parliament_hill);
             }
         }
     };
